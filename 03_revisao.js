560675257eab410fc94ddb4ab573127c3fa5afa1
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2026-08-17 14:48:05 -0300

    Revisão e acumulo de variáveis

---
+++ 03_revisao.js
@@ -0,0 +1,35 @@
+
+let aluguel = 1200.00
+let condominio = 250.59
+let internet = 85.99
+let total = aluguel + condominio + internet
+
+console.log("O total das contas é: "+ total )
+
+
+// -----------------------------------------------------------------------
+
+let nome = "Conrado"
+let sobrenome = "Saud"
+let nome_completo = nome +" "+ sobrenome
+console.log("Seja bem-vindo(a) "+nome_completo)
+
+// -----------------------------------------------------------------------
+
+let salario = 2000
+console.log("Seu salário é R$ "+salario)
+
+let comissao = 100
+salario = salario + comissao // acumulo: adicionar um novo valor na variável
+console.log("Salário com comissão: "+salario)
+
+let adicional_norturno = 200
+salario = salario + adicional_norturno
+console.log("Salário com adicional noturno: "+salario)
+
+/*
+isso é um comentário
+de multiplas linhas
+
+a vontade
+*/
