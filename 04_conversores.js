aae47194e80eaabab986f91a7fbe9d3496e64a59
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2026-08-18 17:35:29 -0300

    Create 04_conversores.js

---
+++ 04_conversores.js
@@ -0,0 +1,29 @@
+// Aula 04: Converter variáveis
+let prompt = require("prompt-sync")()
+
+let numero1 = prompt("Digite um número: ")
+numero1 = parseInt(numero1)
+
+let numero2 = parseInt( prompt("Digite outro número: ") )
+
+let soma = numero1 + numero2
+console.log("O resultado é: "+soma)
+
+// ----------------------------------------
+
+// ----- Atividade 1:
+// Peça para o usuário digitar o ano em que nasceu e
+// mostre a idade atual dela no console
+
+
+// ----- Atividade 2:
+// Peça para o usuário digitar três notas de um aluno
+// em seguida mostre a média escolar dele
+
+
+// ----- Atividade 3:
+// Peça para um usuário digitar:
+// o nome de um produto, o seu preço e a quantidade em estoque
+// Em seguida, exiba o nome do produto e quanto o usuário vai faturar
+// caso venda todos os itens em estoque
+
